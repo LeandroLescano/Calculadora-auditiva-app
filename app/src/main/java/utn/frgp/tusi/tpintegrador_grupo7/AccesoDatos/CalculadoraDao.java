9180150f27cb77d313c50f9f7dacdead8ef10bd2
Juan <49132815+juanccassano@users.noreply.github.com>
2020-11-01 23:42:06 -0300

    Agregada funcion para listar operaciones del historial

--- app/src/main/java/utn/frgp/tusi/tpintegrador_grupo7/AccesoDatos/CalculadoraDao.java
+++ app/src/main/java/utn/frgp/tusi/tpintegrador_grupo7/AccesoDatos/CalculadoraDao.java
@@ -1,4 +1,6 @@
 package utn.frgp.tusi.tpintegrador_grupo7.AccesoDatos;
 
 public class CalculadoraDao {
+
+
 }
--- app/src/main/java/utn/frgp/tusi/tpintegrador_grupo7/AccesoDatos/HistorialDao.java
+++ app/src/main/java/utn/frgp/tusi/tpintegrador_grupo7/AccesoDatos/HistorialDao.java
@@ -1,4 +1,42 @@
 package utn.frgp.tusi.tpintegrador_grupo7.AccesoDatos;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
+import utn.frgp.tusi.tpintegrador_grupo7.Dominio.Color;
+import utn.frgp.tusi.tpintegrador_grupo7.Dominio.Configuracion;
+import utn.frgp.tusi.tpintegrador_grupo7.Dominio.Estado;
+import utn.frgp.tusi.tpintegrador_grupo7.Dominio.Operacion;
+import utn.frgp.tusi.tpintegrador_grupo7.Dominio.Tamano;
+import utn.frgp.tusi.tpintegrador_grupo7.Dominio.Tipografia;
+
 public class HistorialDao {
+
+    Operacion operacion;
+    private ArrayList<Operacion> lista;
+
+    public ArrayList<Operacion> listarOperaciones(Context context){
+        ConexionSQLiteHelper admin = new ConexionSQLiteHelper(context, "db_calculadora", null, 1);
+        SQLiteDatabase BasedeDatos = admin.getWritableDatabase();
+        lista = new ArrayList<>();
+        operacion = new Operacion();
+
+        Cursor ops = BasedeDatos.rawQuery("select id, operacion from historial", null);
+        if(ops.moveToFirst()){
+            while(ops.moveToNext()){
+
+                operacion.setId(Integer.parseInt(ops.getString(0)));
+                operacion.setOperacion(ops.getString(1));
+                lista.add(operacion);
+
+            }
+        }
+        return lista;
+    }
+
+
+
 }
